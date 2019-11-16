79d4d39cd6ce560a33043295e72037f47aead714
yegavintin <51268387+yegavintin@users.noreply.github.com>
2019-11-16 23:23:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pickle
+import json
+from flask import Flask, request, jsonify, render_template
+import os
+
+#setting working directory
+os.chdir("G:\Edwiser material\Project\Cabfarepredition project")
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')   
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features =[int(x)for x in request.form.values()]
+    final_features=[np.array(int_features)]
+    prediction=model.predict(final_features)
+    
+    output=round (prediction[0],2)
+    return render_template('index.html', prediction_text='Fare Amount is ${}'.format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
